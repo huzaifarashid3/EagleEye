1c48a83e16425868097716001dd1aa2f85c33788
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-10-03 19:18:05 +0500

    videos show in a grid

--- webapp.py
+++ webapp.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
-vidoes = []
+st.session_state.videos = []
 
 st.title("EagleEye")
 video_files = st.file_uploader("Upload a Video", accept_multiple_files=True)
 
 if video_files is not None:
     for video_file in video_files:
-        vidoes.append(video_file)
+        st.session_state.videos.append(video_file)
 
-for video in vidoes:
-    st.video(video)
+n_cols = 4
+n_rows = len(st.session_state.videos) // n_cols + 1
+rows = [st.columns(n_cols) for _ in range(n_rows)]
+grid = [cell for row in rows for cell in row]
+
+for cell, video in zip(grid, st.session_state.videos):
+    cell.video(video)
