f3ff412558ec4931ca07bb091aaa6e1ddb248756
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-10-03 22:50:29 +0500

    added tabs for the whole process

--- webapp.py
+++ webapp.py
@@ -1,18 +1,33 @@
 import streamlit as st
 
-st.session_state.videos = []
 
-st.title("EagleEye")
-video_files = st.file_uploader("Upload a Video", accept_multiple_files=True)
+upload = st.expander("Upload Videos",expanded=True)
+select = st.expander("Select Video")
+crop = st.expander("Crop Frame")
+process = st.expander("Process")
 
-if video_files is not None:
-    for video_file in video_files:
-        st.session_state.videos.append(video_file)
 
-n_cols = 4
-n_rows = len(st.session_state.videos) // n_cols + 1
-rows = [st.columns(n_cols) for _ in range(n_rows)]
-grid = [cell for row in rows for cell in row]
+with upload:
+    st.session_state.videos = []
+    st.title("EagleEye")
+    video_files = st.file_uploader("Upload a Video", accept_multiple_files=True)
 
-for cell, video in zip(grid, st.session_state.videos):
-    cell.video(video)
+    if video_files is not None:
+        for video_file in video_files:
+            st.session_state.videos.append(video_file)
+
+
+
+with select:
+    n_cols = 4
+    n_rows = len(st.session_state.videos) // n_cols + 1
+    rows = [st.columns(n_cols) for _ in range(n_rows)]
+    grid = [cell for row in rows for cell in row]
+    for cell, video in zip(grid, st.session_state.videos):
+        cell.video(video)
+
+with crop:
+    st.empty()
+
+with process:
+    st.empty()
