3fbc2110731b4cb7e4dca4081122e621cbcebefc
Huzaifa Rashid <huzaifarashid3@gmail.com>
2024-10-03 23:00:22 +0500

    simplified code (removed not currently needed st.session_state

--- webapp.py
+++ webapp.py
@@ -6,31 +6,30 @@ select = st.expander("Select Video")
 crop = st.expander("Crop Frame")
 process = st.expander("Process")
 
+videos = []
 
 with upload:
-    st.session_state.videos = []
-    st.title("EagleEye")
     video_files = st.file_uploader("Upload a Video", accept_multiple_files=True)
 
     if video_files is not None:
         for video_file in video_files:
-            st.session_state.videos.append(video_file)
+            videos.append(video_file)
 
 
 
 with select:
     n_cols = 4
-    n_rows = len(st.session_state.videos) // n_cols + 1
+    n_rows = len(videos) // n_cols + 1
     rows = [st.columns(n_cols) for _ in range(n_rows)]
     grid = [cell for row in rows for cell in row]
-    for cell, video in zip(grid, st.session_state.videos):
+    for cell, video in zip(grid, videos):
         cell.video(video)
     # TODO: 
     # use st.selectbox to select a video
 
 with crop:
     # TODO:
-    # use some annotation tool to crop the frame
+    # use some annotation tool to crop the frame from the selected video
     st.empty()
 
 with process:
