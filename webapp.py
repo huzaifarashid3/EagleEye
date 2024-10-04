fd28850fbcf0af27549fa5b67ea43eca40d70330
Abdullah Ashar <aasheikh@nisum.com>
2024-10-04 12:45:05 +0500

    implementedd the select section

--- webapp.py
+++ webapp.py
@@ -18,14 +18,26 @@ with upload:
 
 
 with select:
-    n_cols = 4
-    n_rows = len(videos) // n_cols + 1
-    rows = [st.columns(n_cols) for _ in range(n_rows)]
-    grid = [cell for row in rows for cell in row]
-    for cell, video in zip(grid, videos):
-        cell.video(video)
-    # TODO: 
-    # use st.selectbox to select a video
+    if videos:
+        n_cols = 4
+        n_rows = len(videos) // n_cols + 1
+        rows = [st.columns(n_cols) for _ in range(n_rows)]
+        grid = [cell for row in rows for cell in row]
+        for cell, video in zip(grid, videos):
+            cell.video(video)
+        # use st.selectbox to select a video
+        video_names = [f"Video {i+1}" for i in range(len(videos))]
+        selected_video_name = st.selectbox("Select  video to process:", video_names)
+
+        # dddisplay selected viddoe seperatly
+        selected_video_index = video_names.index(selected_video_name)
+        st.write(f"Selected Video: {selected_video_name}")
+        st.video(videos[selected_video_index])
+
+    else:
+        st.write("No videos uploaded yet.")
+
+
 
 with crop:
     # TODO:
